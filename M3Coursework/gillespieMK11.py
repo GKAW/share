2e22ce09deb1986e2e60d0bb53aafd48b1173603
Georg K A Wachter <gkw16@ic.ac.uk>
2019-11-21 10:16:12 +0000

    changes

--- M3Coursework/gillespieMK11.py
+++ M3Coursework/gillespieMK11.py
@@ -8,7 +8,7 @@ import math
 
 #===INITIALISE===
 pltno = 100 # how many gillespie sims do you want?
-endTime = 12000 # When should Gillespie stop? 25k seconds is ca 20 cycles (24000)
+endTime = 1000 # When should Gillespie stop? 25k seconds is ca 20 cycles (24000)
 
 k0 = 0.2  #s^-1
 k1 = 0.01 #s^-1
@@ -165,12 +165,12 @@ sol = scipy.integrate.solve_ivp(dydt_withks, t_span=(0,endTime), y0=(m0,m0), met
 
 
 #===EXECUTE===
-try:
+#try:
 megagil(pltno,t0,endTime,m0) # returns two nested lists (mmegastore and tmegastore) in the format mmegastore[i][j] where i is per Gillepsie simulation and j is the timepoint/mRNA number
-except Exception:
-    print('Could not compute Gillepsie.')
+#except Exception:
+#    print('Could not compute Gillepsie.')
 
-try:
+#try:
 prestats(tmegastore, mmegastore)
 
 stats(linter)
@@ -208,30 +208,30 @@ print('fano new mRNA:',mnewtotfano)
 #    plt.plot(tmegastore[i],mmegastore[i])
 
 mstar = [k0/k1] * 2
-try:
-    plt.plot([0,int(timevec[-1])],mstar,linewidth=1.5,color='r',label="Analytical steady state") # analytical steady state
+#try:
+plt.plot([0,int(timevec[-1])],mstar,linewidth=1.5,color='r',label="Analytical steady state") # analytical steady state
 
-    plt.plot(sol.t,sol.y[0],'k',linewidth=1.5,label="ODE model") #plot the ODE
-    '''
-    plt.plot(tmegastore[0],mmegastore[0],label="Run 1")
-    plt.plot(tmegastore[1],mmegastore[1],label="Run 2")
-    plt.plot(tmegastore[2],mmegastore[2],label="Run 3")
-    plt.plot(tmegastore[3],mmegastore[3],label="Run 4")
-    plt.plot(tmegastore[4],mmegastore[4],label="Run 5")
-    '''
-    plt.plot(timevec,mavgstore,linewidth=1.5,color='b',label="Avergage of 100 Gillespie simulations") # average of all Gillespie sims
+plt.plot(sol.t,sol.y[0],'k',linewidth=1.5,label="ODE model") #plot the ODE
+'''
+plt.plot(tmegastore[0],mmegastore[0],label="Run 1")
+plt.plot(tmegastore[1],mmegastore[1],label="Run 2")
+plt.plot(tmegastore[2],mmegastore[2],label="Run 3")
+plt.plot(tmegastore[3],mmegastore[3],label="Run 4")
+plt.plot(tmegastore[4],mmegastore[4],label="Run 5")
+'''
+plt.plot(timevec,mavgstore,linewidth=1.5,color='b',label="Avergage of 100 Gillespie simulations") # average of all Gillespie sims
 
-    plt.fill_between(timevec,mstdupstore,mstddownstore,facecolor='blue',alpha=0.5, label='Standard deviation around mean')
-    plt.fill_between(timevec,mmaxstore,mminstore,facecolor='blue',alpha=0.3, label='Maxima and minima')
+plt.fill_between(timevec,mstdupstore,mstddownstore,facecolor='blue',alpha=0.5, label='Standard deviation around mean')
+plt.fill_between(timevec,mmaxstore,mminstore,facecolor='blue',alpha=0.3, label='Maxima and minima')
 
 
-    plt.xlabel('time (seconds)')
-    plt.ylabel('mRNA (absolute number)')
-    plt.title('Gillespie simulation of mRNA production')
-    plt.legend(loc="lower right")
-    plt.show()
-except Exception:
-    print('There was an error generating graphs from data. View raw avg data below:')
-    print(['%.2f' % elem for elem in mavgstore]) # also do the redundant formatting here so values are below each other
-    print(tavgstore)
-    print(['%.2f' % elem for elem in tavgstore])
+plt.xlabel('time (seconds)')
+plt.ylabel('mRNA (absolute number)')
+plt.title('Gillespie simulation of mRNA production')
+plt.legend(loc="lower right")
+plt.show()
+#except Exception:
+#    print('There was an error generating graphs from data. View raw avg data below:')
+#    print(['%.2f' % elem for elem in mavgstore]) # also do the redundant formatting here so values are below each other
+    #print(tavgstore)
+#    print(['%.2f' % elem for elem in tavgstore])
